3960df2f562c24b04b79409837e476c8ec67f839
Lucas Dormoy <lucas.dormoy@agicap.com>
2023-05-18 15:10:11 +0200

    feat: rework presentation page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
+import { FormsModule } from "@angular/forms";
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectsComponent } from './projects/projects.component';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeMenuComponent } from './home-menu/home-menu.component';
+import { ProjectsMenuComponent } from './home-menu/projects-menu/projects-menu.component';
+import { HomeComponent } from './home/home.component';
+import { NewsHeaderComponent } from './home/news-header/news-header.component';
+import { NewsItemComponent } from './home/news/news-item/news-item.component';
+import { NewsComponent } from './home/news/news.component';
+import { PresentationComponent } from './presentation/presentation.component';
 import { ProjectsDropdownComponent } from './projects-dropdown/projects-dropdown.component';
-import { HorizonsCaillouxComponent } from './projects/horizons-cailloux/horizons-cailloux.component';
-import { PluieMontchalComponent } from './projects/pluie-montchal/pluie-montchal.component';
-import { WhoamiComponent } from './projects/whoami/whoami.component';
 import { AislingComponent } from './projects/aisling/aisling.component';
-import { SolosForRealComponent } from './projects/solos-for-real/solos-for-real.component';
 import { FluxQuiPortentComponent } from './projects/flux-qui-portent/flux-qui-portent.component';
+import { HorizonsCaillouxComponent } from './projects/horizons-cailloux/horizons-cailloux.component';
 import { KaravelComponent } from './projects/karavel/karavel.component';
 import { OneBreathComponent } from './projects/one-breath/one-breath.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import { HomeMenuComponent } from './home-menu/home-menu.component';
-import { FormsModule } from "@angular/forms";
-import { ProjectsMenuComponent } from './home-menu/projects-menu/projects-menu.component';
-import { HeaderComponent } from './header/header.component';
-import { NewsHeaderComponent } from './home/news-header/news-header.component';
-import { NewsComponent } from './home/news/news.component';
-import { NewsItemComponent } from './home/news/news-item/news-item.component';
+import { PluieMontchalComponent } from './projects/pluie-montchal/pluie-montchal.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SolosForRealComponent } from './projects/solos-for-real/solos-for-real.component';
+import { WhoamiComponent } from './projects/whoami/whoami.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'about', component: AboutComponent},
+  { path: 'about', component: PresentationComponent},
   { path: 'projects', component: ProjectsComponent},
   { path: 'l-horizon-est-fait-de-petits-cailloux', component: HorizonsCaillouxComponent},
   { path: 'dialogue-avec-l-œuvre-pluie-de-montchal', component: PluieMontchalComponent},
@@ -45,7 +45,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
+    PresentationComponent,
     HomeComponent,
     ProjectsComponent,
     ProjectsDropdownComponent,
