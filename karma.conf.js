238910661d7e673b4c76d31d546983e98b4d3bac
Lucas Dormoy <lucas.dormoy@agicap.com>
2023-05-18 11:25:45 +0200

    configure firebase

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/loulou-website'),
+      dir: require('path').join(__dirname, './coverage/compagnie-ezo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
