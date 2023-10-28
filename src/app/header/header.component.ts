d74f37e96eb4af2357cab43715549658d56aa641
Lucas Dormoy <lucas.dormoy@agicap.com>
2023-10-28 12:34:35 +0200

    style: fix header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, HostBinding } from '@angular/core';
+import { distinctUntilChanged, filter, fromEvent, map, pairwise, share, throttleTime } from 'rxjs';
+
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('toggle', [
+      state(
+        VisibilityState.Hidden,
+        style({ 'padding-top': '0rem' })
+      ),
+      state(
+        VisibilityState.Visible,
+        style({ 'padding-top': '2rem' })
+      ),
+      transition('* => *', animate('200ms ease-in')),
+    ]),
+  ],
 })
-export class HeaderComponent implements OnInit {
+
+export class HeaderComponent implements AfterViewInit {
+  private isVisible = true;
+
+  @HostBinding('@toggle')
+  get toggle(): VisibilityState {
+    return this.isVisible ? VisibilityState.Visible : VisibilityState.Hidden;
+  }
 
   constructor() { }
+  ngAfterViewInit(): void {
+    const scroll$ = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.scrollY),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share()
+    );
+
+    const goingUp$ = scroll$.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const goingDown$ = scroll$.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    goingUp$.subscribe(() => (this.isVisible = true));
+    goingDown$.subscribe(() => (this.isVisible = false));
+  }
 
   ngOnInit(): void {
   }
